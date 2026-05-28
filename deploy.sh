ed7f31be701a09052151f15f6cac049d2102c3bc
Rohan Gusinge <rohangusinge1712@gmail.com>
2026-05-28 17:53:03 +0530

    test rollback

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,13 @@ docker pull rohangusinge1712/rohan-devops-app:latest >> /home/ubuntu/deploy.log 
 echo "Updating Kubernetes deployment..." >> /home/ubuntu/deploy.log
 kubectl set image deployment/rohan-app rohan-container=rohangusinge1712/rohan-devops-app:latest >> /home/ubuntu/deploy.log 2>&1
 
+echo "Checking rollout status..." >> /home/ubuntu/deploy.log
+
+kubectl rollout status deployment/rohan-app >> /home/ubuntu/deploy.log 2>&1 || {
+  echo "Deployment failed! Rolling back..." >> /home/ubuntu/deploy.log
+  kubectl rollout undo deployment/rohan-app >> /home/ubuntu/deploy.log 2>&1
+}
+
+echo "Deployment successful!" >> /home/ubuntu/deploy.log
 echo "Done" >> /home/ubuntu/deploy.log
 echo "==============================" >> /home/ubuntu/deploy.log
