f10d5daea30fd108917a7cbad66cb09ef342b13f
Rohan Gusinge <rohangusinge1712@gmail.com>
2026-05-28 16:28:42 +0530

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "Pulling latest image..." >> /home/ubuntu/deploy.log
 docker pull rohangusinge1712/rohan-devops-app:latest >> /home/ubuntu/deploy.log 2>&1
 
 echo "Updating Kubernetes deployment..." >> /home/ubuntu/deploy.log
-kubectl set image deployment/rohan-devops-app rohan-devops-app=rohangusinge1712/rohan-devops-app:latest >> /home/ubuntu/deploy.log 2>&1
+kubectl set image deployment/rohan-app rohan-container=rohangusinge1712/rohan-devops-app:latest >> /home/ubuntu/deploy.log 2>&1
 
 echo "Done" >> /home/ubuntu/deploy.log
 echo "==============================" >> /home/ubuntu/deploy.log
